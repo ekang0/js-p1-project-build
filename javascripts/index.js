6724720d256e4196452e214276d74103170f7682
Esther Kang <eskang87@gmail.com>
2022-02-10 11:09:56 -0800

    Added some logic for home page into index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,48 @@
 //For javascript logic
+
+/* GLOBALS */
+
+
+
+
+
+/* NODE GETTERS */
+const mainDiv = () => document.getElementById("main");
+
+
+
+
+/* TEMPLATES */
+const homePageTemplate = () => {
+  return `
+  <h3>Hello Friend!</h3>
+  <p>Thanks for stopping by our shop.</p>
+  `
+};
+
+
+
+
+/* RENDERS */
+const renderHomePage = () => {
+  mainDiv().innerHTML = homePageTemplate();
+}
+
+
+
+
+/* */
+
+
+
+
+
+
+/* WHEN THE DOM LOADS */
+
+document.addEventListener('DOMContentLoaded',() => {
+  //renderHomePage();
+
+})
+
+
